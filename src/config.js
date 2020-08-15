9b9892693fe3abe6e2789bf7a3111304646d23e9
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-08-15 19:59:34 -0400

    added API info

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 export default {
-  API_ENDPOINT: "http://localhost:8080",
+  API_ENDPOINT: "https://vast-plains-52676.herokuapp.com",
   API_KEY: process.env.REACT_APP_API_KEY,
   API_TOKEN : process.env.API_TOKEN,
   TOKEN_KEY: "Recipe-Token-Key",
   ID_KEY: "user_id",
 };
 // "https://vast-plains-52676.herokuapp.com"
+// "http://localhost:8080"
